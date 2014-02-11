dcb20aa73b694acd2d64089e5fb2474856924a2d
Matthew Hillman <mhillman@bomgar.com>
2014-02-10 18:02:27 -0600

    Initial Commit

---
+++ MyZone/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyZone
+//
+//  Created by Matthew Hillman on 2/10/14.
+//  Copyright (c) 2014 Matthew Hillman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyZone/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyZone
+//
+//  Created by Matthew Hillman on 2/10/14.
+//  Copyright (c) 2014 Matthew Hillman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
